09303a9f2591eea2901ff0e17f20b158b3b68743
enmanuel <enmanuellassis@gmail.com>
2021-11-30 19:36:29 -0400

    Apply code review changes

--- queries.sql
+++ queries.sql
@@ -32,7 +32,7 @@ ROLLBACK;
 /*Inside a transaction:
 Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.*/
 BEGIN;
-UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon%';
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
 /*Update the animals table by setting the species column to pokemon for all animals that don't have species already set.*/
 UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
 /*Commit the transaction.*/
